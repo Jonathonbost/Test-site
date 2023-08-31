8da6937adab99dcd62b8170fd15999e1c4f139f0
Jonathon Bost <wybryant01@gmail.com>
2023-08-31 14:45:19 -0700

    Made a somewhat working search bar wuth basic javascritp

--- homepage/index.js
+++ homepage/index.js
@@ -4,3 +4,19 @@ bigButton.addEventListener("click", function(){
   window.scrollTo(0, 0);
 });
 
+const searchInput = document.getElementById('searchInput');
+const recipeCards = document.querySelectorAll('.recipe');
+
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    
+    recipeCards.forEach(card => {
+        const recipeTitle = card.textContent.toLowerCase();
+        
+        if (recipeTitle.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
