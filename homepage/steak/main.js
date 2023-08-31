0c90a1e4b0cb9a633e667b41518558dba66d9ec8
Jonathon Bost <wybryant01@gmail.com>
2023-08-31 15:46:12 -0700

    that last handful of commtis have been me adding some small javascript features. I am trying to slowlyadd javascript as a learn. I just added an animated images changer

--- homepage/steak/main.js
+++ homepage/steak/main.js
@@ -1,30 +1,38 @@
-// 'js/mian.js'
+// // 'js/mian.js'
 
 let slider_img = document.querySelector('.slider-img');
 let images = [ 'eee.jpg', 'ttt.jpg', 'imrs.webp'];
 let i = 0;
+let clicker = document.getElementById("#uta")
+const body = document.querySelector('body')
 
 function prev(){
+
 	if(i <= 0) i = images.length;	
 	i--;
-	return setImg();			 
+	
+	return setImg(); 
 }
 
-function next(){
+function nxt(){
 	if(i >= images.length-1) i = -1;
 	i++;
 	return setImg();			 
 }
 
+function next(){
+setInterval(() => {
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();
+},1500);
+}
+
+
 function setImg(){
 	return slider_img.setAttribute('src', "images/"+images[i]);
 }
 
-let checkBox = document.querySelector('.hi');
 
+body.onload = next;
 
-document.querySelector(".hi").addEventListener("click", e => {
-	e.preventDefault();
-	hi.classList.remove("hi");
-	hi.classList.add("hi");
-}); // not working yet 
